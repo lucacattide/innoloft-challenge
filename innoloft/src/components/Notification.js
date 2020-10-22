6cb811460a8efde526a4973b81767a9766eb1d11
Luca Cattide <info@lucacattide.dev>
2020-10-22 09:06:15 +0200

    feat: Notification implementation

---
+++ innoloft/src/components/Notification.js
@@ -0,0 +1,30 @@
+// Module Start
+// JS imports
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+// SASS imports
+import '../sass/Notification/notification.scss';
+
+// Notification
+const Notification = (props) => {
+  return (
+    <aside className={clsx(
+      'notification',
+      {
+        ['active']: props.message
+      }
+    )}>
+      <h2 className="notification__title">{props.message}</h2>
+    </aside>
+  );
+};
+
+// Properties validation
+Notification.propTypes = {
+  message: PropTypes.string.isRequired
+};
+
+// Module export
+export default Notification;
+// Module End
