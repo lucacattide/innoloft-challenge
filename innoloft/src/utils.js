e399bece48cffd2898e771a05b7920e7660ee63c
Luca Cattide <info@lucacattide.dev>
2020-10-22 09:07:32 +0200

    feat: Utilities implementation;

---
+++ innoloft/src/utils.js
@@ -0,0 +1,37 @@
+// Module Start
+// Text validation pattern
+const patternText = /^[a-zA-Z0-9àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+// E-Mail validation pattern
+const patternEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+/**
+ * - Minimum eight characters
+ * - at least one uppercase letter
+ * - one lowercase letter
+ * - one number and one special character
+ */
+const patternPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.-_@$!%*?&])[A-Za-z\d.-_@$!%*?&]{8,}$/;
+/**
+ * @description Response status checking
+ * @author Luca Cattide
+ * @date 2020-10-21
+ * @param {object} response Request response
+ * @returns
+ */
+function checkStatus(response) {
+  if ((response.status !== 200)) {
+    const error = new Error(response.statusText);
+
+    return Promise.reject(error);
+  } else {
+    return Promise.resolve(response);
+  }
+}
+
+// Module export
+export {
+  patternText,
+  patternEmail,
+  patternPassword,
+  checkStatus
+};
+// Module End
