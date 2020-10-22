47f75b716133004dbf3c925e661e6e132a719536
Luca Cattide <info@lucacattide.dev>
2020-10-22 11:36:06 +0200

    feat: Menu reducer implementation

--- innoloft/src/reducers/index.js
+++ innoloft/src/reducers/index.js
@@ -1,12 +1,14 @@
 // Module Start
 // JS imports
 import { combineReducers } from 'redux';
+import menu from './menu';
 import profile from './profile';
 import form from './form';
 
 // Reducers
 // Module export
 const appReducer = combineReducers({
+  menu: menu,
   profile: profile,
   form: form
 });
